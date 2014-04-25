bca2f953f9010c6bc0bfd4ba98d67a3ee05c6b09
Martynas <martynas.lycius@gmail.com>
2014-04-25 15:31:32 +0300

    Added two classes TicTacToeApp and TicTacToeGrid for creating app main window

--- TicTacToeApp.py
+++ TicTacToeApp.py
@@ -1,1 +1,14 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 
+
+class TicTacToeApp(App):
+    def build(self):
+        return TicTacToeGrid
+
+
+class TicTacToeGrid(GridLayout):
+    pass
+
+if __name__ == "__main__":
+    TicTacToeApp().run()

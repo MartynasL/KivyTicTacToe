b1086a62d0791604e94d1b385943a0d50881f9a0
Martynas <martynas.lycius@gmail.com>
2014-04-25 15:43:45 +0300

    Added new class TicTacToeCell and created grid from objects of this class

--- TicTacToeApp.py
+++ TicTacToeApp.py
@@ -1,5 +1,11 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.properties import ListProperty
+
+
+class TicTacToeCell(Button):
+    coordinates = ListProperty([0, 0])
 
 
 class TicTacToeApp(App):
@@ -10,7 +16,10 @@ class TicTacToeApp(App):
 class TicTacToeGrid(GridLayout):
     def __init__(self, *args, **kwargs):
         super(TicTacToeGrid, self).__init__(*args, **kwargs)
-
+        for column in range(3):
+            for row in range(3):
+                cell = TicTacToeCell(coordinates=[column, row])
+                self.add_widget(cell)
 
 if __name__ == "__main__":
     TicTacToeApp().run()
